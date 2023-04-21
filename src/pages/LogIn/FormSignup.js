921b46519dfafe96674c7326c6cb7aa1d6a9ba0c
MilanOzaniak <71782252+MilanOzaniak@users.noreply.github.com>
2023-04-21 20:44:44 +0200

    hb

--- src/pages/LogIn/FormSignup.js
+++ src/pages/LogIn/FormSignup.js
@@ -116,7 +116,7 @@ const FormSignup = () => {
         </div>
         <span className='form-input-login'>
             You don´t have an account ? Register  
-              <a href='/HypeStoreFrontend/Register'> here.</a>
+              <a href='/HypeStoreFrontend/register'> here.</a>
         </span>
       </div>
     </div>
